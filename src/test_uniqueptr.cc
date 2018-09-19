c399eef26d48a89b3809ba1c474b8905da266302
Ginevra <ginevracoal@gmail.com>
2018-09-19 17:43:49 +0200

    current status, still problems with fischer meyer

---
+++ src/test_uniqueptr.cc
@@ -0,0 +1,18 @@
+// #include <iostream>
+// #include "../inc/unique_ptr.h"
+
+int main() {
+  //   int* p = new int;
+  //   *p = 6;
+  //   unique_ptr<int> ptr{p};
+  //   unique_ptr<int> u{};
+  //   u = ptr;
+
+  //   ptr.understand();
+
+  //   std::cout << "\np:" << p;
+  //   std::cout << "\n&p:" << &p;
+  //   std::cout << "\n";
+
+  return 0;
+}
